d6f3435d12472391ad17a751302911cce38c127c
kancer <ezeroglukancer@gmail.com>
2020-11-04 16:11:19 +0100

    chnaged the logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/logo-n.svg'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="kancer" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
