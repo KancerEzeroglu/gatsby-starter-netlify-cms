70cb0fdc2bbb9eeefaa495e18021dd39f32d574c
kancer <ezeroglukancer@gmail.com>
2020-11-04 16:14:42 +0100

    chnaged the logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo-n.svg'
+import logo from '../img/logo-n.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
