a884fe5992471e48633ac480d7fe93900cdd7dba
kancer <ezeroglukancer@gmail.com>
2020-11-05 13:14:02 +0100

    changed the logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo-n.png'
+import logo from '../img/logo-2.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
